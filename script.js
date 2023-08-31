1f379cf79654f2f1fd882ab1fcc2180e6a7b68db
Anthony Bosek <anthony.bosek@gmail.com>
2023-08-31 09:53:20 -0700

    new updates

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "I am the developer_ant 🐜🐜🐜";
-// document.querySelector("body").appendChild(h2);
-
-console.log("jello... ??");
-console.log("jello... ??");
-// function toggleMenu() {
-//     const menu = document.querySelector(".menu-links");
-//     const icon = document.querySelector(".hamburger-icon");
-//     menu.classList.toggle("open");
-//     icon.classList.toggle("open");
-//   }
+console.log("developer_ant is connected!!");

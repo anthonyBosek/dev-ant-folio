cc8c96c663b5bd5f6cd38a58d36e92c403fcc145
Anthony Bosek <anthony.bosek@gmail.com>
2023-08-31 12:24:32 -0700

    adds theme toggle

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("developer_ant is connected!!");
+
+let theme = "light";
+const modeSelect = document.getElementById("mode");
+modeSelect.addEventListener("click", function () {
+  if (theme === "light") {
+    newTheme = "dark";
+  } else {
+    newTheme = "light";
+  }
+
+  const elements = document.getElementsByClassName(theme);
+  console.log(elements);
+  for (let i = 0; i < elements.length; i++) {
+    // elements[i].classList.remove(theme);
+    // elements[i].classList.replace(theme, newTheme);
+  }
+
+  if (theme === "light") {
+    theme = "dark";
+  } else {
+    theme = "light";
+  }
+  console.log(theme);
+});

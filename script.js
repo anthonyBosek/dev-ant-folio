96494ee4f643b1277ffd9c9475bee1e75333c61e
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-03 13:13:57 -0700

    completes mode toggle

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const modeToggle = document.getElementById("modeToggle");
-const nav = document.getElementsByTagName("nav");
-const text = document.getElementsByClassName("text");
+const modeToggle = document.getElementById("mode-toggle");
+
 modeToggle.addEventListener("click", function () {
   document.body.classList.toggle("dark");
-  nav[0].classList.toggle("dark");
-  for (let i = 0; i < text.length; i++) {
-    text[i].classList.toggle("dark");
-  }
 });

c5fbbfacbd071740c35be8b1588c8f865f859c24
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-04 19:06:16 -0700

    adds functionality for nav

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const modeToggle = document.getElementById("mode-toggle");
 modeToggle.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
+
+const navItem = document.getElementsByClassName("nav-item");
+
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", function (e) {
+    console.log(e.target);
+  });
+}

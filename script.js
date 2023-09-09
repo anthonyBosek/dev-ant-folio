c604b24074be416069a317efaa8095171210b318
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-08 17:23:29 -0700

    works on typewriter effect

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ modeToggle.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
 
+//
+
 const navItem = document.getElementsByClassName("nav-item");
 const pageList = document.getElementsByClassName("page");
 
@@ -22,3 +24,13 @@ for (let i = 0; i < navItem.length; i++) {
     }
   });
 }
+
+//
+
+const typeWriter = new Typed(".multi-txt", {
+  strings: ["a Student", "a Developer", "a Designer", "an Engineer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
